2f573f37be3a7e4a6c5827d316509474e8ce2155
negash90 <41707814+negash90@users.noreply.github.com>
2018-08-03 09:20:04 -0400

    Chapter 10 Completed

--- app/src/main/java/com/example/nugge/cirminalinetnt/CrimeFragment.java
+++ app/src/main/java/com/example/nugge/cirminalinetnt/CrimeFragment.java
@@ -15,16 +15,28 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import java.util.UUID;
+
 public class CrimeFragment extends Fragment {
+    private static final String ARG_CRIME_ID = "crime_id";
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
 
+    public static CrimeFragment newInstance (UUID crimeId) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_CRIME_ID, crimeId);
+        CrimeFragment fragment = new CrimeFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mCrime = new Crime();
+        UUID crimeId = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
+        mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
     }
 
     @Override
@@ -37,6 +49,7 @@ public class CrimeFragment extends Fragment {
         mDateButton.setEnabled(false);
 
         mSolvedCheckBox = v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setChecked(mCrime.isSolved());
         mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView,
@@ -46,6 +59,7 @@ public class CrimeFragment extends Fragment {
         });
 
         mTitleField = v.findViewById(R.id.crime_title);
+        mTitleField.setText(mCrime.getTitle());
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
--- app/src/main/java/com/example/nugge/cirminalinetnt/CrimeListFragment.java
+++ app/src/main/java/com/example/nugge/cirminalinetnt/CrimeListFragment.java
@@ -1,6 +1,7 @@
 package com.example.nugge.cirminalinetnt;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -41,9 +42,8 @@ public class CrimeListFragment extends Fragment {
         }
      @Override
         public void onClick(View view) {
-         Toast.makeText(getActivity(),
-                 mCrime.getTitle() + "clicked", Toast.LENGTH_SHORT)
-                 .show();
+         Intent intent = CrimeActivity.newIntent(getActivity(),mCrime.getId());
+         startActivity(intent);
      }
     }
 
@@ -89,10 +89,20 @@ public class CrimeListFragment extends Fragment {
 
         return view;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
+    }
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
-        mAdapter = new CrimeAdapter(crimes);
-        mCrimeRecyclerView.setAdapter(mAdapter);
+        if(mAdapter == null) {
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+        } else {
+            mAdapter.notifyDataSetChanged();
+        }
     }
 }
